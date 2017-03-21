b72b9963147c986f8b5a03aa57af3b83d11c0e28
Zachary <hello@zach.ie>
2017-03-21 10:04:20 +0000

    Add GitHub followers to Profile page

--- Handler/Profile.hs
+++ Handler/Profile.hs
@@ -3,32 +3,46 @@ module Handler.Profile where
 import Import
 import Data.Maybe
 
+import GitHub
 import GitHub.Data.Repos
-import qualified GitHub
 import qualified GitHub.Endpoints.Repos as GitHubRepos
+import qualified GitHub.Endpoints.Users.Followers as GitHubFollowers
 
 getProfileR :: Handler Html
 getProfileR = do
     (_, user) <- requireAuthPair
     sess <- getSession
 
-    let login = lookup "login" sess
-    let email = lookup "email" sess
-    let token = lookup "access_token" sess
+    let login = fromJust $ lookup "login" sess
+    let email = fromJust $ lookup "email" sess
+    let token = fromJust $ lookup "access_token" sess
 
-    let githubName = decodeUtf8 $ fromJust login
+    let githubName = decodeUtf8 login
+    let githubUserName = GitHub.mkUserName githubName
     let githubOwnerName = GitHub.mkOwnerName githubName
 
-    possibleRepos <- liftIO $ GitHubRepos.userRepos githubOwnerName GitHub.Data.Repos.RepoPublicityAll
-    repos <- case possibleRepos of
+    -- Get repos for user
+    reposRequest <- liftIO $ GitHubRepos.userRepos githubOwnerName GitHub.Data.Repos.RepoPublicityAll
+    getRepos <- case reposRequest of
         Left e -> error $ show e
-        Right repos -> return repos
+        Right res -> return res
+    let repos = map formatRepo getRepos
 
+    -- Get followers for user
+    followersRequest <- liftIO $ GitHubFollowers.usersFollowing githubUserName
+    getFollowers <- case followersRequest of
+        Left e -> error $ show e
+        Right res -> return res
+    let followers = map formatUser getFollowers
 
 
     defaultLayout $ do
         setTitle . toHtml $ userIdent user <> "'s User page"
         $(widgetFile "profile")
 
+
 formatRepo :: Repo -> Text
-formatRepo repo = GitHubRepos.untagName $ GitHub.Data.Repos.repoName repo
+formatRepo = GitHubRepos.untagName . GitHub.Data.Repos.repoName
+
+formatUser :: SimpleUser -> Text
+formatUser = GitHub.untagName . GitHub.simpleUserLogin
