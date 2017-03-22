3e4e9b61067e129ea74355da6ee9930c252971b0
Zachary <hello@zach.ie>
2017-03-22 16:34:25 +0000

    Add more test endpoints to servant-crawler

--- servant-crawler/src/Crawler.hs
+++ servant-crawler/src/Crawler.hs
@@ -9,6 +9,7 @@ module Crawler
 
 import Data.Aeson
 import Data.Aeson.TH
+import Database.Bolt
 import Network.Wai
 import Network.Wai.Handler.Warp
 import Servant
@@ -23,21 +24,33 @@ data User = User
 
 $(deriveJSON defaultOptions ''User)
 
+-- Type definitions
 type API = "users" :> Get '[JSON] [User]
+           :<|> "albert" :> Get '[JSON] User
+           :<|> "isaac" :> Get '[JSON] User
 
+-- App setup
 startApp :: IO ()
-startApp = run 8080 app
-
+startApp = Network.Wai.Handler.Warp.run 8080 app
 app :: Application
 app = serve api server
-
 api :: Proxy API
 api = Proxy
 
+-- Server
 server :: Server API
 server = return users
+     :<|> return albert
+     :<|> return isaac
 
+-- Handlers
 users :: [User]
 users = [ User 1 "Isaac" "Newton"
         , User 2 "Albert" "Einstein"
         ]
+
+isaac :: User
+isaac = User 1 "Isaac" "Newton"
+
+albert :: User
+albert = User 2 "Albert" "Einstein"
