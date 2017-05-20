728d894dd698ee52f021b5c1b491f99212fa15c9
Zachary <hello@zach.ie>
2017-05-20 21:50:58 +0100

    Improve repo formatting and requests

--- servant-crawler/src/DatabaseAPI.hs
+++ servant-crawler/src/DatabaseAPI.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings      #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module DatabaseAPI where
 
@@ -24,24 +24,18 @@ addUser user = do
                               (fromList [("name", T (fromString user))])
     close pipe
 
-addRepo :: Text -> Repo -> IO()
-addRepo user repo = do
-    let owner = GitHubRepos.repoOwner repo
-    let owner_name = formatName $ simpleOwnerLogin owner
-    let repo_name = formatName $ GitHubRepos.repoName repo
-    logMsg ["Adding Repo: ", owner_name, "/", repo_name, "\n"]
+addRepo :: (Text, Text) -> IO()
+addRepo (owner_name, repo_name) = do
+    logMsg ["Adding Repo: ", unpack owner_name, "/", unpack repo_name, "\n"]
     pipe <- connect config
     result <- run pipe $ queryP "CREATE (n:Repo {owner: {owner}, name: {name}})"
-                                (fromList [("owner", T(fromString owner_name)), ("name", T (fromString repo_name))])
-    result <- run pipe $ query $ Data.Text.pack $ addLink user owner_name repo_name "CONTRIBUTES"
+                                (fromList [("owner", T(owner_name)), ("name", T (repo_name))])
+    result <- run pipe $ query $ Data.Text.pack $ addLink owner_name owner_name repo_name "CONTRIBUTES"
     close pipe
 
-addLink :: Text -> String -> String -> String -> String
-addLink u o r link = "MATCH (u:User {name: '" ++ unpack u ++ "'}) MATCH (r:Repo {owner: '" ++ fromString o ++ "', name: '" ++ fromString r ++ "'}) MERGE (u)-[o:" ++ fromString link ++ "]->(r)"
+addLink :: Text -> Text -> Text -> String -> String
+addLink u o r link = "MATCH (u:User {name: '" ++ unpack u ++ "'}) MATCH (r:Repo {owner: '" ++ unpack o ++ "', name: '" ++ unpack r ++ "'}) MERGE (u)-[o:" ++ fromString link ++ "]->(r)"
 
 -- Utility Functions
-formatName :: Name a -> String
-formatName = unpack . untagName
-
 logMsg :: [String] -> IO()
 logMsg = mapM_ putStr
--- servant-crawler/src/GitHubAPI.hs
+++ servant-crawler/src/GitHubAPI.hs
@@ -6,30 +6,40 @@ import DatabaseAPI
 import Data.Text
 import Data.Vector
 import GitHub
-import GitHub.Data.Repos
+import GitHub.Data
 import qualified GitHub.Endpoints.Users as GitHubUsers
 import qualified GitHub.Endpoints.Repos as GitHubRepos
 import qualified GitHub.Endpoints.Users.Followers as GitHubFollowers
 
 -- Formatting
-formatRepo :: Repo -> String
-formatRepo = formatName . GitHubRepos.repoName
-
 formatUser :: SimpleUser -> Text
 formatUser = untagName . GitHub.simpleUserLogin
 
 formatContributor :: Contributor -> Text
 formatContributor (KnownContributor contributions avatarUrl name url uid gravatar) = GitHub.untagName name
 
+formatRepo :: Repo -> (Text, Text)
+formatRepo repo = do
+    let owner = GitHubRepos.repoOwner repo
+    let owner_name = untagName $ simpleOwnerLogin owner
+    let repo_name = untagName $ GitHubRepos.repoName repo
+    (owner_name, repo_name)
 
 -- Begin Crawl
-crawlUser :: Text -> IO (Vector Repo)
+crawlUser :: Text -> IO (Vector (Text, Text))
 crawlUser user = do
     logMsg ["Crawl started from user: ", unpack user, "\n"]
     repos <- getUserRepos user
-    result <- Data.Vector.mapM (addRepo user) repos
+    result <- Data.Vector.mapM addRepo repos
+    result_two <- Data.Vector.mapM crawlRepo repos
     return repos
 
+crawlRepo :: (Text, Text) -> IO (Vector Contributor)
+crawlRepo (owner, repo) = do
+    logMsg ["Crawl started from repo: ", unpack owner, "/", unpack repo, "\n"]
+    contributors <- getRepoContributors owner repo
+    --result <- Data.Vector.mapM addContributor contributors
+    return contributors
 
 -- API Requests
 getUserInfo :: Text -> IO User
@@ -41,14 +51,14 @@ getUserInfo name = do
         Right res -> return res
     return result
 
-getUserRepos :: Text -> IO (Vector Repo)
+getUserRepos :: Text -> IO (Vector (Text, Text))
 getUserRepos name = do
     let owner = GitHub.mkOwnerName name
-    request <- GitHubRepos.userRepos owner GitHub.Data.Repos.RepoPublicityAll
+    request <- GitHubRepos.userRepos owner RepoPublicityAll
     result <- case request of
         Left e -> error $ show e
         Right res -> return res
-    return $ result
+    return $ Data.Vector.map formatRepo result
 
 getUserFollowers :: Text -> IO (Vector Text)
 getUserFollowers name = do
@@ -59,12 +69,12 @@ getUserFollowers name = do
         Right res -> return res
     return $ Data.Vector.map formatUser result
 
-getRepoContributors :: Text -> Text -> IO (Vector Text)
-getRepoContributors user name = do
-    let owner = GitHub.mkOwnerName user
-    let repo = GitHub.mkRepoName name
-    request <- GitHubRepos.contributors owner repo
+getRepoContributors :: Text -> Text -> IO (Vector Contributor)
+getRepoContributors owner repo = do
+    let github_owner = GitHub.mkOwnerName owner
+    let github_repo = GitHub.mkRepoName repo
+    request <- GitHubRepos.contributors github_owner github_repo
     result <- case request of
         Left e -> error $ show e
         Right res -> return res
-    return $ Data.Vector.map formatContributor result
+    return $ result
